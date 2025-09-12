da83bf5a13a9f21d70cb104776710800362e6501
Cody Roof <codeman2727@gmail.com>
2025-09-12 16:46:30 -0600

    testing about section update for cicd process

--- src/components/About.js
+++ src/components/About.js
@@ -16,13 +16,13 @@ class About extends Component {
           <p className='about-p'>My name is Cody Roof and I'm currently a Data Analyst at Covenant Logistics for my 9 to 5
           and web developer and game developer after hours. <span className='hide-until'>Obsessed with Financial Independence and
           all things geeky and hoping to combine the two into the ultimate combo to achieve my dreams of travelling
-          the world and only working on things that move\excite me.</span>
+          the world and only working on things that move\excite me. Testing this change now with my CICD process</span>
           </p>
         </div>
         <div className='about-desc2'>
           <p className='about-p2'><span className='about-span'>Obsessed </span>with Financial Independence and all things
           geeky and hoping to combine the two into the Ultimate Combo! to achieve my dreams of travelling
-          the world and only working on things that move me or excite me.
+          the world and only working on things that move me or excite me. Testing this too. Is this mobile?
           </p>
         </div>
       </div>
